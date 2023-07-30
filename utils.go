252385c2fc355916ece296249ba2450ee5257404
i3elj <guisande.bernardo@gmail.com>
2023-07-30 15:08:54 -0300

    no more .env file loaded

--- utils.go
+++ utils.go
@@ -1,19 +1,8 @@
 package main
 
 import (
-	"log"
-
-	"github.com/joho/godotenv"
 	"golang.org/x/exp/constraints"
 )
-
-func init() {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-}
 
 // If certain ingredient is found in a list of ingredients, true is returned. False otherwise
 func has_ing(ings []Ingredient, str string) bool {
